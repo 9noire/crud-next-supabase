4f12fa1302a28e068440ac7eb68c42d572f37f06
9noire <rakhaadinata83@gmail.com>
2025-12-17 09:21:11 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import type { IMenu } from "@/types/menu"
+import { useEffect, useState } from "react"
+import supabase from "@/lib/db"
+
+const Home = () => {
+  const [menus, setMenus] = useState<IMenu[]>([])
+
+  useEffect(() => {
+    const fetchMenus = async () => {
+      const {data, error} = await supabase.from('menus').select('*')
+
+      if(error) console.log('error', error)
+      else setMenus(data)
+    }
+
+    fetchMenus()
+  }, [supabase])
+
+  console.log(menus)
+  return (
+    <div>
+      <div>Home</div>
+    </div>
+  )
+}
+
+export default Home
